4ad9c2ceb71c13b553c35e97382b4a90e2a337ef
Nicolas Eugui <74990164+NicoEugui@users.noreply.github.com>
2023-10-18 23:41:02 +0000

    ADD | Se finaliza proyecto de generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// DOMAIN NAME GENERATOR - NICO EUGUI
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronouns = ['the', 'our'];
+let adjetives = ['great', 'big'];
+let nouns = ['jogger', 'racoon', 'internet', 'lastOfUs', 'puedes'];
+let extensions = ['.com', '.net', '.us', '.io', '.es', '.os'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const specialDomain = {
+  "internet": ".net",
+  "puedes": ".es",
+  "lastOfUs": ".os"
 };
+
+
+function generateDomainNames() {
+  let domainNames = [];
+
+  for(let pronoun of pronouns) {
+    for(let adjetive of adjetives) {
+      for(let noun of nouns) {
+        for(let extension of extensions) {
+  
+          let showPronum = pronouns[Math.floor(Math.random() * pronouns.length)];
+          let showAdj = adjetives[Math.floor(Math.random() * adjetives.length)];
+          let showNoun = nouns[Math.floor(Math.random() * nouns.length)];
+          let showExt = extensions[Math.floor(Math.random() * extensions.length)];
+  
+          //console.log(showPronum + showAdj + showNoun + showExt);
+          let newDomain = "";
+
+          if(noun in specialDomain && specialDomain[noun] === extension) {
+            //domainNames.push(`${noun.slice(0,-extension.length+1)}${extension}`);
+            newDomain = `${noun.slice(0, -extension.length + 1)}${extension}`;
+          } else {
+            //domainNames.push(`${showPronum + showAdj + showNoun + showExt}`);
+            newDomain = `${showPronum + showAdj + showNoun + showExt}`;
+          }
+
+          if (!domainNames.includes(newDomain)) {
+            domainNames.push(newDomain);
+          }
+        }
+      }
+    }
+  }
+  return domainNames;
+}
+
+
+function printDomainNames(domainNames) {
+  for (const domain of domainNames) {
+    // eslint-disable-next-line no-console
+    console.log(domain);
+  }
+}
+const domainNames = generateDomainNames();
+printDomainNames(domainNames);
